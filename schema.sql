2118fc5a39bf831aa0caff464270287f96de2363
akuivan <akuivan@users.noreply.github.com>
2020-11-20 13:39:59 +0200

    Update schema.sql

--- schema.sql
+++ schema.sql
@@ -1,4 +1,4 @@
-CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT, password TEXT, admin BOOLEAN);
+CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT, admin BOOLEAN);
 CREATE TABLE categories (id SERIAL PRIMARY KEY, title TEXT);
 CREATE TABLE threads (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, category_id INTEGER REFERENCES categories, title TEXT, private BOOLEAN, created_at TIMESTAMP);
 CREATE TABLE messages (id SERIAL PRIMARY KEY, user_id INTEGER REFERENCES users, thread_id INTEGER REFERENCES threads, content TEXT, sent_at TIMESTAMP);
@@ -6,5 +6,3 @@ CREATE TABLE allowedusers (id SERIAL PRIMARY KEY, thread_id INTEGER REFERENCES t
 INSERT INTO categories (title) VALUES ('ruoka');
 INSERT INTO categories (title) VALUES ('ohjelmointi');
 INSERT INTO categories (title) VALUES ('muu');
-
-
